d91b3a9f0ad312ef8727a057c7ed3e0ed0a3d3aa
kamranss <kamranss@code.edu.az>
2023-01-31 23:35:45 +0400

    Refactor: testing repo

--- DataAccess/Repositories/EmployeeRepository.cs
+++ DataAccess/Repositories/EmployeeRepository.cs
@@ -30,6 +30,7 @@ namespace DataAccess.Repositories
             List<Employee> employeelist = new List<Employee>();
             employeelist = JsonConvert.DeserializeObject<List<Employee>>(jsonformatedEmployeeFromFile);
            
+            // Checking whether the updating repository is working or not
             //employeelist.Add(employeelist);
             employeelist.Add(entity);
             var jsonFormatedEmployee = JsonConvert.SerializeObject(employeelist);
@@ -54,34 +55,34 @@ namespace DataAccess.Repositories
 
         public bool Delete(Employee entity)
         {
-            string filePath = @"C:\Users\kamra\Desktop\Files\Database.json";
+            //string filePath = @"C:\Users\kamra\Desktop\Files\Database.json";
 
-            var jsonformatedEmployeeFromFile = File.ReadAllText(filePath);
-            List<Employee> employeelist = new List<Employee>();
-            employeelist = JsonConvert.DeserializeObject<List<Employee>>(jsonformatedEmployeeFromFile);
+            //var jsonformatedEmployeeFromFile = File.ReadAllText(filePath);
+            //List<Employee> employeelist = new List<Employee>();
+            //employeelist = JsonConvert.DeserializeObject<List<Employee>>(jsonformatedEmployeeFromFile);
 
-      
 
-            if (employeelist.Find(entity) != null)
-            {
-                employeelist.Remove(entity);
-                return employeelist;
-            }
 
-            var jsonFormatedEmployee = JsonConvert.SerializeObject(employeelist);
-            if (System.IO.File.Exists(filePath) != false)
-            {
-                System.IO.File.WriteAllText(filePath, jsonFormatedEmployee);
-            }
-            else
-            {
-                System.IO.File.Delete(filePath);
-            }
+            //if (employeelist.Find(entity) != null)
+            //{
+            //    employeelist.Remove(entity);
+            //    return employeelist;
+            //}
 
-            if (jsonFormatedEmployee != null)
-            {
-                return true;
-            }
+            //var jsonFormatedEmployee = JsonConvert.SerializeObject(employeelist);
+            //if (System.IO.File.Exists(filePath) != false)
+            //{
+            //    System.IO.File.WriteAllText(filePath, jsonFormatedEmployee);
+            //}
+            //else
+            //{
+            //    System.IO.File.Delete(filePath);
+            //}
+
+            //if (jsonFormatedEmployee != null)
+            //{
+            //    return true;
+            //}
             return false;
         }
 
